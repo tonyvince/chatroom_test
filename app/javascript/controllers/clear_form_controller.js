027537e2213fe54a236670cdb29540b3aa193d2c
Tony Yesudas <tony3vincent@icloud.com>
2024-03-25 12:03:26 +0100

    Submit message on enter

--- app/javascript/controllers/clear_form_controller.js
+++ app/javascript/controllers/clear_form_controller.js
@@ -4,4 +4,11 @@ export default class extends Controller {
   clear() {
     this.element.reset()
   }
+
+  submitOnEnter(event) {
+    if (event.key === "Enter" && !event.shiftKey) { // Added !event.shiftKey to allow Shift+Enter for line breaks if needed
+      event.preventDefault();
+      this.element.requestSubmit();
+    }
+  }
 }
